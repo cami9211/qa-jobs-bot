7bd3d4ef0932fabde12d0c9b4728ccf79be62ad7
cami9211 <camilolearm03@gmail.com>
2026-08-26 10:51:46 -0500

    Update GEMINI_URL to point to Google homepage

--- find_qa_jobs_gemini.py
+++ find_qa_jobs_gemini.py
@@ -23,8 +23,8 @@ EMAIL_APP_PASSWORD = os.environ["EMAIL_APP_PASSWORD"]
 EMAIL_TO = os.environ["EMAIL_TO"]
 
 GEMINI_URL = (
-    "https://generativelanguage.googleapis.com/v1beta/models/"
-    "gemini-flash-latest:generateContent"
+    "https://www.google.com/"
+    
 )
 
 PROMPT = (
